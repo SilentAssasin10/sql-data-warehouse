567e1980c62d348368953845ac3d521db70591a6
Arkapravo Roy <darklordxdlm10@gmail.com>
2026-06-03 01:27:35 +0530

    deleted ddl commands of init database
    Updated the marital status column name in the customer table.

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,43 +1,3 @@
-/*
-=====================================================
-CREATE DATABASE AND SCHEMA
-=====================================================
-
-Purpose: Writing this script to create a the bronze layer of the database Datawarehouse and if it exists, then drop the whole
-database and create a new one, and also create three tables or schemas named bronze, silver and gold.
-
-WARNING: If this script is run, the whole database gets deleted and it creates a fresh database.
-So all the data will be lost if there is no backup and rollback is possible there.
-*/
-
-
-USE master;
-Go
-
--- Drop and create new database if the database already exists
-
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
-BEGIN 
-	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-	DROP DATABASE DataWarehouse;
-	END;
-
-Go
-
-/* Create the schemas into the database */
-
-CREATE DATABASE Datawarehouse;
-Go
-
-USE DataWarehouse;
-GO
-
-CREATE SCHEMA bronze;
-GO
-CREATE SCHEMA silver;
-Go
-CREATE SCHEMA gold;
-Go
 
 /*
 ==============================================
@@ -54,7 +14,7 @@ CREATE TABLE bronze.crm_cust_info (
 	cst_key NVARCHAR (50),
 	cst_firstname NVARCHAR(50),
 	cst_lastname NVARCHAR(50),
-	cst_material_status NVARCHAR(50),
+	cst_marital_status NVARCHAR(50),
 	cst_gndr NVARCHAR(50),
 	cst_create_date DATE
 );
